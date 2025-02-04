5c8a78a1012863028a7989daf86b4642c4eb8070
Puc3k <kacperplocienniczak20@gmail.com>
2025-02-04 22:45:23 +0100

    feat: add meal not found page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,15 @@
 import classes from "./page.module.css";
 import {getMeal} from "@/lib/meals";
 import Image from 'next/image'
+import {notFound} from "next/navigation";
 
 export default function MealDetailsPage({params}) {
     const meal = getMeal(params.slug)
+
+    if (!meal) {
+        notFound()
+    }
+
     meal.instructions = meal.instructions.replace(/\n/g, '<br />')
     return (
         <>
